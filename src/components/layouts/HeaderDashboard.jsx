1ecbe18cdaa8466e7cb168d7cf690d5bb7b42883
pinaki-17 <pinakimitr17@gmail.com>
2025-07-05 10:57:50 +0530

    all clear

--- src/components/layouts/HeaderDashboard.jsx
+++ src/components/layouts/HeaderDashboard.jsx
@@ -71,6 +71,24 @@ const Header = () => {
         <i className="bi bi-list toggle-sidebar-btn"></i>
       </div>
 
+      <div className="search-bar">
+        <form
+          className="search-form d-flex align-items-center"
+          method="POST"
+          action="#"
+        >
+          <input
+            type="text"
+            name="query"
+            placeholder="Search"
+            title="Enter search keyword"
+          />
+          <button type="submit" title="Search">
+            <i className="bi bi-search"></i>
+          </button>
+        </form>
+      </div> 
+
       <nav className="header-nav ms-auto">
         <ul className="d-flex align-items-center">
           <li className="nav-item d-block d-lg-none">
